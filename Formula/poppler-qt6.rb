39ef79da9bfde7e308f6779359b97741f6e14406
Tingkuo Zhang <zhangtingkuo@gmail.com>
2023-12-28 23:44:47 +0800

    Add poppler for qt6
    Signed-off-by: Tingkuo Zhang <zhangtingkuo@gmail.com>

---
+++ Formula/poppler-qt6.rb
@@ -0,0 +1,84 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class PopplerQt6 < Formula
+  desc "PDF rendering library (based on the xpdf-3.0 code base)"
+  homepage "https://poppler.freedesktop.org/"
+  url "https://poppler.freedesktop.org/poppler-23.12.0.tar.xz"
+  sha256 "beba398c9d37a9b6d02486496635e08f1df3d437cfe61dab2593f47c4d14cdbb"
+  license "GPL-2.0-only"
+  head "https://gitlab.freedesktop.org/poppler/poppler.git", branch: "master"
+
+  # depends_on "cmake" => :build
+  depends_on "cmake" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "pkg-config" => :build
+  depends_on "cairo"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "gpgme"
+  depends_on "jpeg-turbo"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "little-cms2"
+  depends_on "nspr"
+  depends_on "nss"
+  depends_on "openjpeg"
+  depends_on "qt"
+
+  uses_from_macos "gperf" => :build
+  uses_from_macos "curl", since: :monterey # 7.68.0 required by poppler as of https://gitlab.freedesktop.org/poppler/poppler/-/commit/8646a6aa2cb60644b56dc6e6e3b3af30ba920245
+  uses_from_macos "zlib"
+
+  conflicts_with "pdftohtml", "pdf2image", "xpdf",
+    because: "poppler, pdftohtml, pdf2image, and xpdf install conflicting executables"
+
+  fails_with gcc: "5"
+
+  resource "font-data" do
+    url "https://poppler.freedesktop.org/poppler-data-0.4.12.tar.gz"
+    sha256 "c835b640a40ce357e1b83666aabd95edffa24ddddd49b8daff63adb851cdab74"
+  end
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    ENV.cxx11
+
+    # removes /usr/include from CFLAGS (not clear why)
+    ENV["PKG_CONFIG_SYSTEM_INCLUDE_PATH"] = "/usr/include" if OS.mac? && MacOS.version < :mojave
+
+    args = std_cmake_args + %W[
+      -DBUILD_GTK_TESTS=OFF
+      -DENABLE_BOOST=OFF
+      -DENABLE_CMS=lcms2
+      -DENABLE_GLIB=ON
+      -DENABLE_QT5=OFF
+      -DENABLE_QT6=ON
+      -DENABLE_UNSTABLE_API_ABI_HEADERS=ON
+      -DWITH_GObjectIntrospection=ON
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build_shared", *args
+    system "cmake", "--build", "build_shared"
+    system "cmake", "--install", "build_shared"
+
+    system "cmake", "-S", ".", "-B", "build_static", *args, "-DBUILD_SHARED_LIBS=OFF"
+    system "cmake", "--build", "build_static"
+    lib.install "build_static/libpoppler.a"
+    lib.install "build_static/cpp/libpoppler-cpp.a"
+    lib.install "build_static/glib/libpoppler-glib.a"
+
+    resource("font-data").stage do
+      system "make", "install", "prefix=#{prefix}"
+    end
+  end
+
+  test do
+    system bin/"pdfinfo", test_fixtures("test.pdf")
+  end
+end
